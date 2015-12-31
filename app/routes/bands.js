4382b81e96fbc2b76a3e47e5d6a03d8add9624c0
Tom Wayson <tom@tomwayson.com>
2015-12-31 10:26:47 -0800

    master-detail

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -2,17 +2,63 @@ import Ember from 'ember';
 
 var Band = Ember.Object.extend({
   name: '',
+  slug: Ember.computed('name', function() {
+    return this.get('name').dasherize();
+  })
 });
+
+var Song = Ember.Object.extend({
+  title: '',
+  raiting: 0,
+  band: ''
+});
+
+var blackDog = Song.create({
+  title: 'Black Dog',
+  rating: 3,
+  band: 'Led Zepplin'
+});
+
+var yellowLedbetter = Song.create({
+  title: 'Yellow Ledbetter',
+  band: 'Pearl Jam',
+  rating: 4
+});
+
+var daughter = Song.create({
+  title: 'Daughter',
+  band: 'Pearl Jam',
+  rating: 5
+});
+
+var pretender = Song.create({
+  title: 'The Pretender',
+  band: 'Foo Fighters',
+  rating: 2
+});
+
+var BandCollection = Ember.Object.extend({
+  content: [],
+  sortProperties: ['name:desc'],
+  sortedContent: Ember.computed.sort('content', 'sortProperties')
+});
+
 var ledZeppelin = Band.create({
-  name: 'Led Zeppelin'
+  name: 'Led Zeppelin',
+  songs: [blackDog]
 });
 var pearlJam = Band.create({
-  name: 'Pearl Jam'
+  name: 'Pearl Jam',
+  songs: [daughter, yellowLedbetter]
 });
 var fooFighters = Band.create({
-  name: 'Foo Fighters'
+  name: 'Foo Fighters',
+  songs: [pretender]
 });
-var bands = [ledZeppelin, pearlJam, fooFighters];
+
+// var bands = [ledZeppelin, pearlJam, fooFighters];
+var bands = BandCollection.create();
+bands.get('content').pushObjects([ledZeppelin, pearlJam, fooFighters]);
 
 export default Ember.Route.extend({
   model: function() {
