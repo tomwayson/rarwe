9d7011f2b1296919f3737f25fac54f2f8cf0ccc3
Tom Wayson <tom@tomwayson.com>
2015-12-31 09:14:00 -0800

    routes

---
+++ app/routes/songs.js
@@ -0,0 +1,42 @@
+import Ember from 'ember';
+
+var Song = Ember.Object.extend({
+  title: '',
+  raiting: 0,
+  band: ''
+});
+
+var blackDog = Song.create({
+  title: 'Black Dog',
+  rating: 3,
+  band: 'Led Zepplin'
+});
+
+var yellowLedbetter = Song.create({
+  title: 'Yellow Ledbetter',
+  band: 'Pearl Jam',
+  rating: 4
+});
+var pretender = Song.create({
+  title: 'The Pretender',
+  band: 'Foo Fighters',
+  rating: 2
+});
+
+var SongCollection = Ember.Object.extend({
+  content: [],
+  sortProperties: ['rating:desc'],
+  sortedContent: Ember.computed.sort('content', 'sortProperties')
+});
+
+var songs = SongCollection.create();
+songs.get('content').pushObject(blackDog);
+songs.get('content').pushObject(yellowLedbetter);
+songs.get('content').pushObject(pretender);
+
+
+export default Ember.Route.extend({
+  model: function() {
+    return songs;
+  }
+});
