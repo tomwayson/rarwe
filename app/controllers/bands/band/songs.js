2871ea43fe20bc7c6230a33f0c66801791fee6cf
Tom Wayson <tom@tomwayson.com>
2015-12-31 11:59:47 -0800

    disable add buttons for empty inputs

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  title: '',
+
+  isAddButtonDisabled: Ember.computed('title', function() {
+    return Ember.isEmpty(this.get('title'));
+  }),
+
   actions: {
     updateRating: function(params) {
       var song = params.item,
