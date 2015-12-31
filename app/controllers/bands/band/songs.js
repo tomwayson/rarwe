23954e0542c0827900eed4fece33d8924a6db064
Tom Wayson <tom@tomwayson.com>
2015-12-31 12:17:55 -0800

    new band/song workflow

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -3,11 +3,20 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   title: '',
 
+  songCreationStarted: false,
+
   isAddButtonDisabled: Ember.computed('title', function() {
     return Ember.isEmpty(this.get('title'));
   }),
 
+  canCreateSong: Ember.computed('songCreationStarted', 'model.songs.length', function() {
+    return this.get('songCreationStarted') || this.get('model.songs.length');
+  }),
+
   actions: {
+    enableSongCreation: function() {
+      this.set('songCreationStarted', true);
+    },
     updateRating: function(params) {
       var song = params.item,
         rating = params.rating;
